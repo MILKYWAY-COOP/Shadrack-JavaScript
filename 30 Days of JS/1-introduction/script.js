fae9589dc104f0b378aa308c84b6b08c8ad87d15
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-02-27 05:21:34 +0300

    first commits this year

---
+++ 30 Days of JS/1-introduction/script.js
@@ -0,0 +1,9 @@
+console.log("Hello, word")
+
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+
+document.getElementById('ss').innerHTML= 'Hello this is my second day finishing my yesterdays works';
+
+document.getElementById('ssd').innerHTML= 'It always good to know that consinstency always outpower intensity';
+
+document.getElementById("demo").innerHTML = "Hello JavaScript";
